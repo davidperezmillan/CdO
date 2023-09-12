3287b5118d786fce51949cc86ac0d88caea19f03
davidperezmillan <david.perez.millan@gmail.com>
2023-09-12 11:37:13 +0200

    delete init

--- CdOInfoShowService/src/main/java/com/davidperezmillan/cdoinfoshowservice/application/usecases/SearchInfoUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.davidperezmillan.cdoinfoshowservice.application.usecases;
-
-import com.davidperezmillan.cdoinfoshowservice.domain.model.Serie;
-
-public interface SearchInfoUseCase {
-
-    Serie[] search(Serie search);
-
-}
