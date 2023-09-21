090883570a9498f547fc911320646ced786d1fbb
davidperezmillan <david.perez.millan@gmail.com>
2023-09-21 19:35:36 +0200

    disable cors registry

--- CdOInfoShowService/src/main/java/com/davidperezmillan/cdoinfoshowservice/infraestructure/config/CorsConfig.java
+++ CdOInfoShowService/src/main/java/com/davidperezmillan/cdoinfoshowservice/infraestructure/config/CorsConfig.java
@@ -9,9 +9,9 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost").allowedOrigins("http://localhost:8080")
+/*        registry.addMapping("/**").allowedOrigins("http://localhost")
                 .allowedOrigins("http://192.168.1.195").allowedMethods("GET", "POST", "PUT", "DELETE");
-        // Agrega aquí los encabezados permitidos .allowCredentials(true);
+        // Agrega aquí los encabezados permitidos .allowCredentials(true);*/
 
     }
 
