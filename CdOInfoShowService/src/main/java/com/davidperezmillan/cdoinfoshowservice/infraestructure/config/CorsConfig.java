65ffdc09cc2c6e33187054a99afb7b270f8ddf2b
davidperezmillan <david.perez.millan@gmail.com>
2023-09-21 11:30:51 +0200

    enable filter isSerie

--- CdOInfoShowService/src/main/java/com/davidperezmillan/cdoinfoshowservice/infraestructure/config/CorsConfig.java
+++ CdOInfoShowService/src/main/java/com/davidperezmillan/cdoinfoshowservice/infraestructure/config/CorsConfig.java
@@ -1,17 +1,24 @@
 package com.davidperezmillan.cdoinfoshowservice.infraestructure.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    /*
-     * @Override public void addCorsMappings(CorsRegistry registry) {
-     * registry.addMapping("/**").allowedOrigins("http://localhost:8080") // Agrega aquí los orígenes permitidos
-     * .allowedMethods("GET", "POST", "PUT", "DELETE") // Agrega aquí los métodos HTTP permitidos .allowedHeaders("*")
-     * // Agrega aquí los encabezados permitidos .allowCredentials(true);
-     *
-     * }
-     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:8080").allowedOrigins("http://192.168.1.195") // Agrega
+                                                                                                                  // aquí
+                                                                                                                  // los
+                                                                                                                  // orígenes
+                                                                                                                  // permitidos
+                .allowedMethods("GET", "POST", "PUT", "DELETE"); // Agrega aquí los métodos HTTP permitidos
+                                                                 // .allowedHeaders("*")
+
+        // Agrega aquí los encabezados permitidos .allowCredentials(true);
+
+    }
+
 }
