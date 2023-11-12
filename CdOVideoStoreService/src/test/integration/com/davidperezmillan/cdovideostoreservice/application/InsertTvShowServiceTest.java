bc2e97ab37b4cac187459d9bd5cde6760e31294d
David Perez Millan <david.perez.millan@gmail.com>
2023-11-12 10:18:27 +0100

    Reformat test

---
+++ CdOVideoStoreService/src/test/integration/com/davidperezmillan/cdovideostoreservice/application/InsertTvShowServiceTest.java
@@ -0,0 +1,10 @@
+package com.davidperezmillan.cdovideostoreservice.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class InsertTvShowServiceTest {
+
+}
