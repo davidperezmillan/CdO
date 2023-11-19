53d655379c26ce99022ef95cde0919f2938e3090
David Perez Millan <david.perez.millan@gmail.com>
2023-11-19 17:38:44 +0100

    refactor sheduled text

--- CdOVideoStoreService/src/main/java/com/davidperezmillan/cdovideostoreservice/config/ScheduledService.java
+++ CdOVideoStoreService/src/main/java/com/davidperezmillan/cdovideostoreservice/config/ScheduledService.java
@@ -1,11 +1,15 @@
 package com.davidperezmillan.cdovideostoreservice.config;
 
 import com.davidperezmillan.cdovideostoreservice.application.services.InsertTvShowService;
+import com.davidperezmillan.cdovideostoreservice.infrastructure.rest.dtos.TvShowResponse;
 import com.davidperezmillan.cdovideostoreservice.infrastructure.slack.SlackService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -23,7 +27,10 @@ public class ScheduledService {
     public void ejecutarTarea() {
         // Lógica de tu tarea programada
         log.info("Tarea programada ejecutada");
-        SlackService.sendMessage("Tarea programada ejecutada");
-        insertTvShowService.addPremieres();
+        List<TvShowResponse> nuevos = insertTvShowService.addPremieres();
+        String texto = nuevos.stream()
+                .map(TvShowResponse::getTitle)
+                .collect(Collectors.joining("/n"));
+        SlackService.sendMessage("Tarea programada ejecutada: "+ texto);
     }
 }
